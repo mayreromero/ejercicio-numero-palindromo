f33a18e7b267b6909223e707e88493188bcb8224
Mayre <mayrer20@gmail.com>
2020-08-24 21:10:52 -0300

    empezando el ejercicio

---
+++ palindromo.js
@@ -0,0 +1,40 @@
+// EJERCICIO NUMERO PALINDROMO
+
+let array_numero = [1, 8, 3, 4];
+let array_creciente = [];
+let array_decreciente = [];
+let j = array_numero.length -1;
+
+for (let i = 0; i < array_numero.length; i++) {
+    array_creciente = array_numero[i];
+    console.log(array_creciente); 
+};
+while(j >= 0){
+    array_decreciente = array_numero[j]
+    j--;
+    console.log(array_decreciente);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
